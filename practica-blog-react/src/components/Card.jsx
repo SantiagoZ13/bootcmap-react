e3b610b3aa9a3cce57596a89abe26e68b64afdd3
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2024-08-02 18:13:11 -0500

    I'm creating components in this commit I create the card component, missing other (links, button, etc)

--- practica-blog-react/src/components/Card.jsx
+++ practica-blog-react/src/components/Card.jsx
@@ -1,3 +1,6 @@
+import './Card.scss'
+import classnames from 'classnames'
+
 function Tag({children, link = '#'}){
     return(
         <span className="tag tag-primary">
@@ -6,19 +9,24 @@ function Tag({children, link = '#'}){
     )
 }
 
-function Card({unsplashID, unsplashAlt, title, extract, author, date}) {
+function Card({unsplashID, unsplashAlt, tags, title, extract, date, author, readTime, isExtended = false, isReversed = false}) {
     return (
-        <article className="card">
+        <article className={classnames("card", {
+            "is-extended": isExtended,
+            "is-reverse": isReversed
+        })}>
         <img src={`https://images.unsplash.com/${unsplashID}`} alt={unsplashAlt} />
         <section>
             <header>
 
                 <div className="tags">
-                    <Tag>Web Components</Tag>
-                    <Tag>CSS</Tag>
-                    
+                    {tags.map((tag)=> <Tag>{tag}</Tag>)}
                 </div>
                 <h2 className="heading heading-sm font-bold">{title}</h2>
+                
+                <time className="help-text paragraph paragraph-xs font-regular" dateTime={date}>
+                    {date}
+                </time>
             </header>
             <p className="paragraph paragraph-sm font-regular">{extract}</p>
             <footer>
@@ -26,9 +34,7 @@ function Card({unsplashID, unsplashAlt, title, extract, author, date}) {
                     <img src="https://i.pravatar.cc/100" alt="Autor del post" />
                     <span className="paragraph paragraph-xs font-bold">{author}</span>
                 </div>
-                <time className="help-text paragraph paragraph-xs font-regular" dateTime={date}>
-                    {date}
-                </time>
+                <p className="help-text paragraph paragraph-xs font-regular">{readTime}</p>
             </footer>
         </section>
 

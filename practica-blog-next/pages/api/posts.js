efff11fccf0910a5874c1eb3852879a23815de3e
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2024-08-15 12:58:06 -0500

    Changed the fake api and reduce the text words of the body card

--- practica-blog-next/pages/api/posts.js
+++ practica-blog-next/pages/api/posts.js
@@ -4,16 +4,21 @@ function calculateReadTime(text){
   return `${time} min`
 }
 
+function reduceWords(text){
+  const newText = text.substr(0, 220)
+  return newText
+}
+
 export default async function handler(req, res) {
-  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then((data) => data.json())
+  const {posts} = await fetch("https://dummyjson.com/posts").then((data) => data.json())
   
   const mappedPost = posts.map((post) => ({
 
     "unsplashID": "photo-1498050108023-c5249f4df085?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "unsplashAlt": "Web Components",
-    "tags": ["Design", "Web Components"],
+    "tags": post.tags,
     "title": post.title,
-    "extract": post.body,
+    "extract": reduceWords(post.body),
     "author": "JSON Placeholder",
     "date": "2024-08-01",
     "readTime": calculateReadTime(post.body)

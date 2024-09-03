db51a4ccdff30c71e059ac0832aa01df3edac1bd
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2024-09-03 18:42:35 -0500

    Create schema, model, and routes for the post CRUD

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,21 +1,26 @@
-import express from "express"
 import dotenv from "dotenv"
-import mongoose, { mongo } from "mongoose"
-
 dotenv.config()
+
+import express from "express"
+import mongoose, { mongo } from "mongoose"
+import { postRouter } from "./posts/router.js"
+
+
 
 const app = express()
 const {PORT, MONGO_USERNAME, MONGO_HOSTNAME, MONGO_PASSWORD} = process.env
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/?retryWrites=true&w=majority&appName=Cluster`; 
 
-
+// Globals Middlewares
 app.use(express.json())
 
-app.get("/", (req, res)=>{
-    res.send("API is running")
-})
+// Routes
+app.get("/", (req, res) => res.send("API is running"))
+app.use("/posts", postRouter)
 
 app.listen(PORT, () => { 
     console.log(` 🌎 Server is running in port: ${PORT}`)
-    mongoose.connect(MONGO_URI)
+    mongoose.connect(MONGO_URI, {
+        dbName: "sample_training"
+    })
 })
